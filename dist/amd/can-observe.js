ccfa238c2d1e0c0c6e9593806a2b36967ea0989a
Matthew Phillips <matthew@matthewphillips.info>
2017-02-02 20:59:49 -0500

    0.1.1

---
+++ dist/amd/can-observe.js
@@ -0,0 +1,37 @@
+/*can-observe@0.1.0#can-observe*/
+define(function (require, exports, module) {
+    var cid = require('can-cid');
+    var canEvent = require('can-event');
+    var canBatch = require('can-event/batch');
+    var Observation = require('can-observation');
+    var has = Object.prototype.hasOwnProperty;
+    module.exports = function (obj) {
+        cid(obj);
+        obj.addEventListener = canEvent.addEventListener;
+        obj.removeEventListener = canEvent.removeEventListener;
+        var p = new Proxy(obj, {
+            get: function (target, name) {
+                if (name !== '_cid' && has.call(target, name)) {
+                    Observation.add(target, name);
+                }
+                return target[name];
+            },
+            set: function (target, key, value) {
+                var old = target[key];
+                var change = old !== value;
+                if (change) {
+                    target[key] = value;
+                    canBatch.dispatch.call(target, {
+                        type: key,
+                        target: target
+                    }, [
+                        value,
+                        old
+                    ]);
+                }
+                return true;
+            }
+        });
+        return p;
+    };
+});
