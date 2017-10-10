69b6bdd28ff45941d7db1110bdf02704b97e2f1c
Bradley Momberger <brad@bitovi.com>
2017-10-10 01:24:10 -0400

    Make getters and setters observable just like values

--- can-observe.js
+++ can-observe.js
@@ -5,24 +5,37 @@ var canSymbol = require("can-symbol");
 var canReflect = require("can-reflect");
 
 var observableSymbol = canSymbol.for("can.observeData");
+var hasOwn = Object.prototype.hasOwnProperty;
 
 var proxyOnly = {};
 canReflect.assignSymbols(proxyOnly, {
 	"can.onKeyValue": function(key, handler) {
 		var handlers = this[observableSymbol].handlers;
 		var keyHandlers = handlers[key];
+		var descriptor = Object.getOwnPropertyDescriptor(this, key);
 		if (!keyHandlers) {
 			keyHandlers = handlers[key] = [];
 		}
-		keyHandlers.push(handler);
+		if(descriptor && descriptor.get) {
+			var obs = new Observation(descriptor.get, this[observableSymbol].proxy, handler);
+			keyHandlers.push(obs);
+			obs.start();
+		} else {
+			keyHandlers.push(handler);
+		}
 	},
 	"can.offKeyValue": function(key, handler) {
 		var handlers = this[observableSymbol].handlers;
 		var keyHandlers = handlers[key];
 		if(keyHandlers) {
-			var index = keyHandlers.indexOf(handler);
+			var index = keyHandlers.map(function(h) {
+				return h.compute && h.compute.updater || h;
+			}).indexOf(handler);
 			if(index >= 0 ) {
-				keyHandlers.splice(index, 1);
+				var obs = keyHandlers.splice(index, 1);
+				if(typeof obs[0].stop === "function") {
+					obs[0].stop();
+				}
 			}
 		}
 	}
@@ -43,31 +56,43 @@ var observe = function(obj){
 			if(proxyOnly[key]) {
 				return proxyOnly[key];
 			}
-
-			var value = target[key];
-			if (!canReflect.isSymbolLike(key) && !canReflect.isObservableLike(value) && canReflect.isPlainObject(value)) {
-				target[key] = observe(value);
+			var descriptor = Object.getOwnPropertyDescriptor(target, key);
+			var value;
+			if(descriptor && descriptor.get) {
+				value = descriptor.get.call(receiver);
+			} else {
+				value = target[key];
 			}
-			if (key !== "_cid" && (key in target || !Object.isSealed(target))) {
+			if (!canReflect.isSymbolLike(key) && !canReflect.isObservableLike(value) && canReflect.isPlainObject(value)) {
+				value = target[key] = observe(value);
+			}
+			if (key !== "_cid" && (hasOwn.call(target, key) || !Object.isSealed(target))) {
 				Observation.add(receiver, key.toString());
 			}
-			return target[key];
+			return value;
 		},
-		set: function(target, key, value){
-			var old = target[key];
-			var change = old !== value;
+		set: function(target, key, value, receiver){
+			var old, change;
+			var descriptor = Object.getOwnPropertyDescriptor(target, key);
+			if (!canReflect.isSymbolLike(key) && !canReflect.isObservableLike(value) && canReflect.isPlainObject(value)) {
+				value = observe(value);
+			} else if (value[observableSymbol]){
+				value = value[observableSymbol].proxy;
+			}
 
-			if (change) {
-				if (!canReflect.isSymbolLike(key) && !canReflect.isObservableLike(value) && canReflect.isPlainObject(value)) {
-					target[key] = observe(value);
-				} else if (value[observableSymbol]){
-					target[key] = value[observableSymbol].proxy;
-				} else {
+			if(descriptor && descriptor.set) {
+				descriptor.set.call(receiver, value);
+			} else {
+				old = target[key];
+				change = old !== value;
+				if (change) {
 					target[key] = value;
 				}
+			}
+			if(descriptor && descriptor.set || change) {
 				(target[observableSymbol].handlers[key] || []).forEach(function(handler){
-					canBatch.queue([handler, this, [value, old]]);
-				}, this);
+					canBatch.queue([handler.get ? handler.get.bind(handler) : handler, receiver, [value, old]]);
+				});
 			}
 			return true;
 		},
