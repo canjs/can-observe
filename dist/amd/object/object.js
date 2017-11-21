cd1127c9976f34beb96c50aa5fcbceb440d96a7c
Justin Meyer <justinbmeyer@gmail.com>
2017-11-21 14:36:16 -0600

    2.0.0-pre.9

---
+++ dist/amd/object/object.js
@@ -0,0 +1,78 @@
+/*can-observe@2.0.0-pre.8#object/object*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-reflect',
+    'can-symbol',
+    '../src/-make-object',
+    '../src/-make-observe',
+    'can-event-queue/map',
+    'can-event-queue/type',
+    'can-queues',
+    '../src/-helpers'
+], function (require, exports, module) {
+    var canReflect = require('can-reflect');
+    var canSymbol = require('can-symbol');
+    var makeObject = require('../src/-make-object');
+    var makeObserve = require('../src/-make-observe');
+    var eventMixin = require('can-event-queue/map');
+    var typeEventMixin = require('can-event-queue/type');
+    var queues = require('can-queues');
+    var helpers = require('../src/-helpers');
+    var definitionsSymbol = canSymbol.for('can.typeDefinitions');
+    var metaSymbol = canSymbol.for('can.meta');
+    function ensureTypeDefinition(obj) {
+        var typeDefs = obj.prototype[definitionsSymbol];
+        if (!typeDefs) {
+            typeDefs = obj.prototype[definitionsSymbol] = Object.create(null);
+        }
+        return typeDefs;
+    }
+    var ObserveObject = function (props) {
+        var prototype = Object.getPrototypeOf(this);
+        var constructor = this.constructor;
+        var instance = this;
+        var definitions = prototype[definitionsSymbol] || {};
+        for (var key in definitions) {
+            Object.defineProperty(instance, key, definitions[key]);
+        }
+        if (props) {
+            canReflect.assign(instance, props);
+        }
+        return makeObject.observable(instance, {
+            observe: makeObserve.observe,
+            proxyKeys: { constructor: constructor }
+        });
+    };
+    typeEventMixin(ObserveObject);
+    canReflect.assignSymbols(ObserveObject, {
+        'can.defineInstanceKey': function (prop, value) {
+            ensureTypeDefinition(this)[prop] = value;
+        },
+        'can.dispatchInstanceBoundChange': function (obj, isBound) {
+            var meta = this[metaSymbol];
+            if (meta) {
+                var lifecycleHandlers = meta.lifecycleHandlers;
+                if (lifecycleHandlers) {
+                    queues.enqueueByQueue(lifecycleHandlers.getNode([]), this, [
+                        obj,
+                        isBound
+                    ]);
+                }
+            }
+        }
+    });
+    ObserveObject.extend = helpers.makeSimpleExtender(ObserveObject);
+    eventMixin(ObserveObject.prototype);
+    Object.defineProperty(ObserveObject.prototype, 'disconnectedCallback', {
+        enumerable: false,
+        writable: true,
+        configurable: true,
+        value: function () {
+            this.off();
+            this.stopListening();
+        }
+    });
+    module.exports = ObserveObject;
+});
---
+++ dist/amd/src/-symbols.js
@@ -0,0 +1,13 @@
+/*can-observe@2.0.0-pre.8#src/-symbols*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-symbol'
+], function (require, exports, module) {
+    var canSymbol = require('can-symbol');
+    module.exports = {
+        metaSymbol: canSymbol.for('can.meta'),
+        patchesSymbol: canSymbol('patches')
+    };
+});
