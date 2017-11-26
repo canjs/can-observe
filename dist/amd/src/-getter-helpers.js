462d320a7fea8cf85c879a5d4750e556596856b2
Justin Meyer <justinbmeyer@gmail.com>
2017-11-26 14:56:15 -0600

    2.0.0-pre.14

---
+++ dist/amd/src/-getter-helpers.js
@@ -0,0 +1,92 @@
+/*can-observe@2.0.0-pre.13#src/-getter-helpers*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-symbol',
+    'can-event-queue/type',
+    'can-reflect',
+    './-memoize-getter',
+    'can-queues'
+], function (require, exports, module) {
+    var canSymbol = require('can-symbol');
+    var definitionsSymbol = canSymbol.for('can.typeDefinitions');
+    var typeEventMixin = require('can-event-queue/type');
+    var canReflect = require('can-reflect');
+    var memoizeGetter = require('./-memoize-getter');
+    var queues = require('can-queues');
+    var computedDefinitionsSymbol = canSymbol.for('can.computedDefinitions');
+    var metaSymbol = canSymbol.for('can.meta');
+    var getterHelpers = {
+        ensureTypeDefinition: function (obj) {
+            var typeDefs = obj.prototype[definitionsSymbol];
+            if (!typeDefs) {
+                typeDefs = obj.prototype[definitionsSymbol] = Object.create(null);
+            }
+            return typeDefs;
+        },
+        shouldRecordObservationOnAllKeysExceptFunctionsOnProto: function (keyInfo, meta) {
+            return meta.preventSideEffects === 0 && !keyInfo.isAccessor && (keyInfo.targetHasOwnKey || !keyInfo.protoHasKey && !Object.isSealed(meta.target) || keyInfo.protoHasKey && typeof targetValue !== 'function');
+        },
+        addMethodsAndSymbols: function (Type) {
+            typeEventMixin(Type);
+            canReflect.assignSymbols(Type, {
+                'can.defineInstanceKey': function (prop, value) {
+                    getterHelpers.ensureTypeDefinition(this)[prop] = value;
+                },
+                'can.dispatchInstanceBoundChange': function (obj, isBound) {
+                    var meta = this[metaSymbol];
+                    if (meta) {
+                        var lifecycleHandlers = meta.lifecycleHandlers;
+                        if (lifecycleHandlers) {
+                            queues.enqueueByQueue(lifecycleHandlers.getNode([]), this, [
+                                obj,
+                                isBound
+                            ]);
+                        }
+                    }
+                }
+            });
+        },
+        setupComputedProperties: function (prototype) {
+            var computed = {};
+            Object.getOwnPropertyNames(prototype).forEach(function (prop) {
+                var descriptor = Object.getOwnPropertyDescriptor(prototype, prop);
+                if (descriptor.get !== undefined) {
+                    var getObservationData = memoizeGetter.memoize(prototype, prop, descriptor);
+                    computed[prop] = getObservationData;
+                }
+            });
+            return computed;
+        },
+        addMemoizedGetterBindings: function (proxyKeys) {
+            return canReflect.assignSymbols(proxyKeys, {
+                'can.onKeyValue': function (key, handler, queue) {
+                    var getObservation = this[computedDefinitionsSymbol][key];
+                    if (getObservation !== undefined) {
+                        memoizeGetter.bind(getObservation(this));
+                    }
+                    var handlers = this[metaSymbol].handlers;
+                    handlers.add([
+                        key,
+                        queue || 'notify',
+                        handler
+                    ]);
+                },
+                'can.offKeyValue': function (key, handler, queue) {
+                    var getObservation = this[computedDefinitionsSymbol][key];
+                    if (getObservation !== undefined) {
+                        memoizeGetter.unbind(getObservation(this));
+                    }
+                    var handlers = this[metaSymbol].handlers;
+                    handlers.delete([
+                        key,
+                        queue || 'notify',
+                        handler
+                    ]);
+                }
+            });
+        }
+    };
+    module.exports = getterHelpers;
+});
